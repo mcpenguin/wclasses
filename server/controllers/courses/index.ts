b4aacb21d4431bbbd42f5323d14223c3f8dec099
Marcus Chan <marcuschan@geotab.com>
2022-06-13 12:13:58 -0400

    added route for course description

--- server/controllers/courses/index.ts
+++ server/controllers/courses/index.ts
@@ -1,1 +1,19 @@
+import { Response, Request } from "express"
+import Course from "../../models/Course"
+import ICourse from "../../types/ICourse"
+
+export const GetInfoForCourse = async (req: Request, res: Response):
+Promise<void> => {
+    try {
+        const course: null | ICourse = await Course.findOne({
+            "subjectCode": req.params.subjectCode,
+            "catalogNumber": req.params.catalogNumber
+        });
+        res.status(200).json({course});
+    }
+    catch (err) {
+        res.status(400).json({err});
+    }
+}
+
 
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express"
 
 import * as classes from "../controllers/classes";
+import * as courses from "../controllers/courses";
 
 const router: Router = Router();
+
+router.get("/courses/details/:subjectCode/:catalogNumber", 
+courses.GetInfoForCourse);
 
 router.get("/courses/schedule/:subjectCode/:catalogNumber", classes.GetScheduleForCourse);
 router.get("/courses/schedule/:subjectCode/:catalogNumber/:termCode", classes.GetScheduleForCourseWithTermCode);
