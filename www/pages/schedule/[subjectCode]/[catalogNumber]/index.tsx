10accb114a0314e07d58a0cc9c8bc3fc706a20cf
mcpenguin <marcus.chanwc@gmail.com>
2022-06-20 22:03:20 -0400

    [intermediate commit] attempt to get courses

--- www/pages/schedule/[subjectCode]/[catalogNumber]/index.tsx
+++ www/pages/schedule/[subjectCode]/[catalogNumber]/index.tsx
@@ -5,25 +5,56 @@ import Link from 'next/link'
 import styles from './Schedule.module.scss'
 
 import ICourseWithUWFlowData from '@server/types/ICourseWithUWFlowData'
+// import ICourse from '../../../../../server/types/ICourse'
 import ICourseCode from '@server/types/ICourseCode'
 
 import { parseTermCode } from '../../../../helpers/termcode'
 
+const GetCourse = async (course: ICourseCode) => {
+  const { subjectCode, catalogNumber } = course;
+  let response = await fetch(`http://localhost:8000/courses/details/${subjectCode}/${catalogNumber}`)
+  let c = await response.json();
+  return c;
+}
+
+const TurnICourseIntoLink = (c: any) => {
+  return <p>
+    <Link href={`/schedule/${c.subjectCode}/${c.catalogNumber}`}>
+      <a>
+        {c.subjectCode} {c.catalogNumber} - {c.title}
+      </a>
+    </Link>
+  </p>
+}
+
 // This function gets called at build time on server-side
 export async function getServerSideProps(context: any) {
-  const {subjectCode, catalogNumber} = context.params;
+  const subjectCode: string = context.params.subjectCode;
+  const catalogNumber: string = context.params.catalogNumber;
   let response = null;
 
-  response = await fetch(`http://localhost:8000/courses/details/${subjectCode}/${catalogNumber}/`);
-  const courseDetails: ICourseCode[] = await response.json();
+  response = await fetch(`http://localhost:8000/courses/details/${subjectCode.toUpperCase()}/${catalogNumber.toUpperCase()}/`);
+  const courseDetails = await response.json();
 
-  response = await fetch(`http://localhost:8000/courses/termOfferings/${subjectCode}/${catalogNumber}/`);
+  response = await fetch(`http://localhost:8000/courses/termOfferings/${subjectCode.toUpperCase()}/${catalogNumber.toUpperCase()}/`);
   const termOfferingsDetails: ICourseCode[] = await response.json();
+
+  let corequisites = courseDetails.corequisites
+    ?.map(c => await GetCourse(c))
+
+  let antirequisites = courseDetails.antirequisites
+    ?.map(c => await GetCourse(c))
+
+  let postrequisites = courseDetails.postrequisites
+    ?.map(c => await GetCourse(c))
 
   return {
     props: {
       courseDetails,
       termOfferingsDetails,
+      corequisites: [corequisites ?? [null]][0],
+      antirequisites: [antirequisites ?? [null]][0],
+      postrequisites: [postrequisites ?? [null]]
     },
   }
 }
@@ -31,26 +62,22 @@ export async function getServerSideProps(context: any) {
 const Schedule: NextPage = (props: any) => {
 
   let courseDetails: ICourseWithUWFlowData = props.courseDetails;
-  let courseName: string = `${courseDetails.subjectCode} ${courseDetails.catalogNumber} - ${courseDetails.title}`
-
-  let corequisitesAsLinks = courseDetails.corequisites
-    ?.map(c => <p>
-      <Link href={`/schedule/${c.subjectCode}/${c.catalogNumber}`}>
-        <a>{c.subjectCode} {c.catalogNumber} - {courseDetails.title}</a>
-      </Link>
-    </p>)
-
-  let antirequisitesAsLinks = courseDetails.antirequisites
-  ?.map(c => <p>
-    <Link href={`/schedule/${c.subjectCode}/${c.catalogNumber}`}>
-      <a>{c.subjectCode} {c.catalogNumber} - {courseDetails.title}</a>
-    </Link>
-  </p>)
+  let courseCode: string = `${courseDetails.subjectCode} ${courseDetails.catalogNumber}`;
+  let courseName: string = `${courseCode} - ${courseDetails.title}`
 
   let termOfferingsDetails: string[] = props.termOfferingsDetails;
 
   let termCodesAsElements = termOfferingsDetails
     .map(t => <li>{parseTermCode(t)}</li>);
+
+  let corequisites = props.corequisites;
+  let corequisitesAsLinks = corequisites.map(TurnICourseIntoLink)
+
+  let antirequisites = props.antirequisites;
+  let antirequisitesAsLinks = antirequisites.map(TurnICourseIntoLink)
+
+  let postrequisites = props.postrequisites;
+  let postrequisitesAsLinks = postrequisites.map(TurnICourseIntoLink)
 
   return (
     <div className={styles.container}>
@@ -67,12 +94,15 @@ const Schedule: NextPage = (props: any) => {
 
         <h3>Prerequisites</h3>
         <p>{courseDetails.prerequisitesAsString}</p>
-        
+
         <h3>Corequisites</h3>
         {corequisitesAsLinks}
 
         <h3>Antirequisites</h3>
         {antirequisitesAsLinks}
+
+        <h3>Courses {courseCode} leads to</h3>
+        {postrequisitesAsLinks}
 
         <h3>Terms Offered in the Past</h3>
         <ul>
@@ -85,3 +115,4 @@ const Schedule: NextPage = (props: any) => {
 }
 
 export default Schedule;
+
