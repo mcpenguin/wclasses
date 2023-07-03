1c0fe042aeca2924b6bae84f6a2fbcaa70696526
mcpenguin <marcus.chanwc@gmail.com>
2023-07-02 22:55:00 -0400

    added try/catch to schedule

--- web/components/schedule.tsx
+++ web/components/schedule.tsx
@@ -12,29 +12,34 @@ export default function Schedule({ scheduleData }: {scheduleData: any}) {
     <th>Instructor</th>
   </tr>
   const rows = scheduleData.map((row: any) => {
-    let prof = ['',''];
-    if (row.instructor){
-      prof = row.instructor.split(',');
+    try {
+      let prof = ['',''];
+      if (row.instructor){
+        prof = row.instructor.split(',');
+      }
+      const profName = `${prof[1]} ${prof[0]}`;
+      let timeString;
+      let dayString;
+      if (row.time) {
+        timeString = `${new Time(row.time.startTime).toString()} - ${new Time(row.time.endTime).toString()} `;
+        dayString = row.time.days.join(',')
+      } else {
+        timeString = '';
+        dayString = '';
+      }
+      return (<tr key={row.classNumber}>
+        <td className={styles.section}>{row.section.type} {row.section.num}</td>
+        <td className={styles.classNumber}>{row.classNumber}</td>
+        <td className={styles.enrolled}>{row.enrolTotal} / {row.enrolCap}</td>
+        <td className={styles.time}>{timeString}</td>
+        <td className={styles.day}>{dayString}</td>
+        <td className={styles.room}>{row.buildingCode} {row.roomNumber}</td>
+        <td className={styles.prof}>{profName}</td>
+      </tr>)
+    } catch (e) {
+      console.log(e);
+      return <tr></tr>
     }
-    const profName = `${prof[1]} ${prof[0]}`;
-    let timeString;
-    let dayString;
-    if (row.time) {
-      timeString = `${new Time(row.time.startTime).toString()} - ${new Time(row.time.endTime).toString()} `;
-      dayString = row.time.days.join(',')
-    } else {
-      timeString = '';
-      dayString = '';
-    }
-    return (<tr key={row.classNumber}>
-      <td className={styles.section}>{row.section.type} {row.section.num}</td>
-      <td className={styles.classNumber}>{row.classNumber}</td>
-      <td className={styles.enrolled}>{row.enrolTotal} / {row.enrolCap}</td>
-      <td className={styles.time}>{timeString}</td>
-      <td className={styles.day}>{dayString}</td>
-      <td className={styles.room}>{row.buildingCode} {row.roomNumber}</td>
-      <td className={styles.prof}>{profName}</td>
-    </tr>)
   })
   return (
     <div className={styles.container}>
